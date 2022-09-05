e87daad95f5aa503fac0cc71f85d0fcfbce13751
jhones <jhon.rodriguez22329@ucaldas.edu.co>
2022-09-05 17:13:37 -0500

    first commit

---
+++ src/models/cargo.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cargo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+
+  constructor(data?: Partial<Cargo>) {
+    super(data);
+  }
+}
+
+export interface CargoRelations {
+  // describe navigational properties here
+}
+
+export type CargoWithRelations = Cargo & CargoRelations;
