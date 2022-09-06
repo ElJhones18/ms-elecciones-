ad64d7bb943d79cf5fb961b7c81c50049f1e7189
jhones <jhon.rodriguez22329@ucaldas.edu.co>
2022-09-05 19:22:12 -0500

    creacion de relaciones

--- src/repositories/movimiento.repository.ts
+++ src/repositories/movimiento.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
 import {MysqlDataSource} from '../datasources';
-import {Movimiento, MovimientoRelations} from '../models';
+import {Movimiento, MovimientoRelations, Candidato} from '../models';
+import {CandidatoRepository} from './candidato.repository';
 
 export class MovimientoRepository extends DefaultCrudRepository<
   Movimiento,
   typeof Movimiento.prototype.id,
   MovimientoRelations
 > {
+
+  public readonly condidatos: HasManyRepositoryFactory<Candidato, typeof Movimiento.prototype.id>;
+
   constructor(
-    @inject('datasources.mysql') dataSource: MysqlDataSource,
+    @inject('datasources.mysql') dataSource: MysqlDataSource, @repository.getter('CandidatoRepository') protected candidatoRepositoryGetter: Getter<CandidatoRepository>,
   ) {
     super(Movimiento, dataSource);
+    this.condidatos = this.createHasManyRepositoryFactoryFor('condidatos', candidatoRepositoryGetter,);
+    this.registerInclusionResolver('condidatos', this.condidatos.inclusionResolver);
   }
 }
